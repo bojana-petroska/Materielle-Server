093286fa30f810c34a892d1150e02e2a18f1cd41
bojana-petroska <bojana.petroska@yahoo.com>
2023-05-17 18:43:51 +0200

    first try to deploy

--- config/index.js
+++ config/index.js
@@ -35,4 +35,10 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(
+    cors({
+      origin: [FRONTEND_URL]
+    })
+  )
 };
+
